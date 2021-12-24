12d7d6a91dd2264563bda08c9f04e9f15e39c6c9
GukMaria <guk.maria@bk.ru>
2021-12-24 17:38:11 +0300

    fixed mistakes

--- src/Singolo/index.js
+++ src/Singolo/index.js
@@ -15,41 +15,42 @@ const buttonNext = document.querySelector('.slider__arrow--right');
 
 const slider = document.getElementById('slider');
 const slides = document.querySelectorAll('.slider__item');
-let CURRENTSLIDE = 0;
+let currentSlide = 0;
 let enabled = true;
+let numberOfSlide = 0;
 
-function changeCurrentSlide(n) {
-  CURRENTSLIDE = (n + slides.length) % slides.length;
+function changeCurrentSlide(numberOfSlide) {
+  currentSlide = (numberOfSlide + slides.length) % slides.length;
 };
 
 function hideSlide(direction) {
   enabled = false;
-  slides[CURRENTSLIDE].classList.add(direction);
-  slides[CURRENTSLIDE].addEventListener('animationend', function () {
+  slides[currentSlide].classList.add(direction);
+  slides[currentSlide].addEventListener('animationend', function () {
     this.classList.remove('slider__item--home', direction);
   })
 };
 
 function showSlide(direction) {
-  slides[CURRENTSLIDE].classList.add('slider__item--next', direction);
-  slides[CURRENTSLIDE].addEventListener('animationend', function () {
+  slides[currentSlide].classList.add('slider__item--next', direction);
+  slides[currentSlide].addEventListener('animationend', function () {
     this.classList.remove('slider__item--next', direction);
     this.classList.add('slider__item--home');
     enabled = true;
   })
 };
 
-function previousSlide(n) {
+function previousSlide(numberOfSlide) {
   hideSlide('to-right');
-  changeCurrentSlide(n - 1);
+  changeCurrentSlide(numberOfSlide - 1);
   showSlide('from-left');
   slider.classList.toggle("slider__second");
   document.querySelector('.slider__second--next').classList.add('block');
 };
 
-function nextSlide(n) {
+function nextSlide(numberOfSlide) {
   hideSlide('to-left');
-  changeCurrentSlide(n + 1)
+  changeCurrentSlide(numberOfSlide + 1)
   showSlide('from-right');
   slider.classList.toggle("slider__second");
   document.querySelector('.slider__second--next').classList.add('block');
@@ -57,13 +58,13 @@ function nextSlide(n) {
 
 buttonPrev.addEventListener('click', () => {
   if (enabled) {
-    previousSlide(CURRENTSLIDE)
+    previousSlide(currentSlide)
   }
 });
 
 buttonNext.addEventListener('click', () => {
   if (enabled) {
-    nextSlide(CURRENTSLIDE)
+    nextSlide(currentSlide)
   }
 });
 
